cc706ae8e0fa6bd967df8d083a6b9d9ec9834b5d
Martyn Gigg <martyn.gigg@gmail.com>
2019-10-31 14:24:28 +0000

    Add scripts to enable/disable a list of jobs

---
+++ disable-jobs.groovy
@@ -0,0 +1,13 @@
+/**
+ * Disable specified jobs.
+ *
+ * Required variable bindings:
+ *   jobNames - A comma delimited list of projects to check
+ */
+def jenkins = jenkins.model.Jenkins.instance;
+def chosenJobs = jenkins.items.findAll{job -> jobNames.contains(job.name)};
+
+chosenJobs.each { job ->
+  println "Disabling job " + job.name;
+  job.disable()
+}
---
+++ enable-jobs.groovy
@@ -0,0 +1,13 @@
+/**
+ * Enable specified jobs.
+ *
+ * Required variable bindings:
+ *   jobNames - A comma delimited list of projects to check
+ */
+def jenkins = jenkins.model.Jenkins.instance;
+def chosenJobs = jenkins.items.findAll{job -> jobNames.contains(job.name)};
+
+chosenJobs.each { job ->
+  println "Enabling job " + job.name;
+  job.enable()
+}
